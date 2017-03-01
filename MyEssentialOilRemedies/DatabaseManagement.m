494a59290b25ff00b695b9ac2ebe56ceabf83529
Joe Mireles <bs.GitHub@burnsoft.net>
2017-02-28 20:16:40 -0500

    comment out last attempt, changed version to 1.0.49 - non published version

--- MyEssentialOilRemedies/DatabaseManagement.m
+++ MyEssentialOilRemedies/DatabaseManagement.m
@@ -9,6 +9,44 @@
 #import "DatabaseManagement.h"
 
 @implementation DatabaseManagement
+/*
++ (BOOL)addSkipBackupAttributeToItemAtURL:(NSURL *)URL  {
+    if (&NSURLIsExcludedFromBackupKey == nil) { // iOS <= 5.0.1
+        const char* filePath = [[URL path] fileSystemRepresentation];
+        
+        const char* attrName = "com.apple.MobileBackup";
+        u_int8_t attrValue = 1;
+        
+        int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
+        return result == 0;
+    } else { // iOS >= 5.1
+        NSError *error = nil;
+        [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:&error];
+        return error == nil;
+    }
+}
+*/
++ (void)copyDatabaseIfNeeded:(NSString *) getDBPath DBName:(NSString *) databaseName
+{
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+    NSString *toPath = getDBPath;
+    BOOL success = [fileManager fileExistsAtPath:toPath];
+    
+    if(!success) {
+        NSString *fromPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
+        
+        NSURL *toURL = [NSURL fileURLWithPath:toPath];
+        NSURL *fromURL = [NSURL fileURLWithPath:fromPath];
+        success = [fileManager copyItemAtURL:fromURL toURL:toURL error:&error];
+        
+        if (success) {
+            //[self addSkipBackupAttributeToItemAtURL:toURL];
+        } else {
+            NSLog(@"Failed to create writable database file with message '%@'.", [error localizedDescription]);
+        }
+    }   
+}
 
 -(BOOL) backupDatabaseToiCloudByDBName:(NSString *) DBNAME LocalDatabasePath:(NSString *) dbPathString ErrorMessage:(NSString **) msg
 {
