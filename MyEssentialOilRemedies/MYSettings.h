0c0038d8155a9fc993b1d00d201cc5e455b32ce7
Joe M <joe.mireles@burnsoft.net>
2018-08-31 20:55:27 -0400

    #52 submitted changes for testing

--- MyEssentialOilRemedies/MYSettings.h
+++ MyEssentialOilRemedies/MYSettings.h
@@ -10,7 +10,7 @@
 
 #define MYDBNAME "MEO.db"       //Database Name
 #define MYDBVERSION 1.3         //Expected Database Version for the current version of this app
-static BOOL BUGGERME = YES;
+static BOOL BUGGERME = NO;
 //#warning TODO: ON RELEASE COMPILE LITE AND REGULAR VERSION
 static BOOL ISLITE = NO;
 static int LITE_LIMIT = 15;
