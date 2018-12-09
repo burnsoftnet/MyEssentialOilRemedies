ac51275ca9a083a9786a2aa8853147471350ddd9
Joe M <joe.mireles@burnsoft.net>
2018-12-09 14:03:05 -0500

    #59 Added Comments to Math Class

--- MyEssentialOilRemedies/include/BurnSoftGeneral/BurnSoftMath.m
+++ MyEssentialOilRemedies/include/BurnSoftGeneral/BurnSoftMath.m
@@ -10,95 +10,130 @@
 
 @implementation BurnSoftMath
 #pragma mark Get Price Per Item
-//Pass the price and qty to have it broken down to Price per item
-//returns a double with two decimal
+/*!
+    @brief Pass the price and qty to have it broken down to Price per item
+    @return returns a double with two decimal
+ */
 -(double)getPricePerItem :(NSString *) price :(NSString *) qty
 {
     return [[NSString stringWithFormat:@"%.2f",[price doubleValue]/[qty doubleValue]] doubleValue];
 }
 
 #pragma mark Multiply Two Items
-//Pass two values to have them multiplied
+/*!
+    @brief Pass two values to have them multiplied
+    @return
+ */
 -(int) multiplyTwoItems :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [itemOne intValue] * [itemTwo intValue];
 }
 
 #pragma markMultiply Two Items as Double Two Decimals
-//Pass two values to have them multiplied
-//returns a double with two decimal
+/*!
+ @brief Pass two values to have them multiplied
+ @return returns a double with two decimal
+ */
 -(double) multiplyTwoItems2DecDouble :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [[NSString stringWithFormat:@"%.2f",[itemOne doubleValue] * [itemTwo doubleValue]] doubleValue];
 }
 
 #pragma markMultiply Two Items as Double
-//Pass two values to have them multiplied
-//returns as full double value
+/*!
+ @brief Pass two values to have them multiplied
+ @return returns as full double value
+ */
 -(double) multiplyTwoItemsFullDouble :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [itemOne doubleValue] * [itemTwo doubleValue];
 }
 
 #pragma mark Add two Values as Integer
-//Pass two string values to return as intenger
+/*!
+ @brief Pass two string values to return as intenger
+ @return
+ */
 -(int) AddTwoItemsAsInteger :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [itemOne intValue] + [itemTwo intValue];
 }
 
 #pragma mark Add Two Values as Double
-//Pass Two string values to return as double
+/*!
+ @brief Pass Two string values to return as double
+ @return
+ */
 -(double) AddTwoItemsAsDouble :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [itemOne doubleValue] + [itemTwo doubleValue];
 }
 
 #pragma mark Convert Double To String
-//Convert a Double Value to a string with two decimals
+/*!
+ @brief Convert a Double Value to a string with two decimals
+ @return
+ */
 -(NSString *)convertDoubleToString :(double) dValue
 {
     return [NSString stringWithFormat:@"%f",dValue];
 }
 
 #pragma mark Get Price per item as String
-//Pass the price and qty to have it broken down to Price per item
-//returns a string with two decimal
+/*!
+ @brief Pass the price and qty to have it broken down to Price per item
+ @return returns a string with two decimal
+ */
 -(NSString *)getPricePerItemString :(NSString *) price :(NSString *) qty
 {
     return [NSString stringWithFormat:@"%.2f",[price doubleValue]/[qty doubleValue]];
 }
 
 #pragma mark Multiply Two Strings to Return String
-//Pass two string values to multiply and return the value as string
+/*!
+ @brief Pass two string values to multiply and return the value as string
+ @return
+ */
 -(NSString *) multiplyTwoItemsString :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [NSString stringWithFormat:@"%d",[itemOne intValue] * [itemTwo intValue]];
 }
 
 #pragma mark  Multiply Two Strings to Return Double Two Decimals
-//Pass two string values to multiply and return the value as stwo decimal point double
+/*!
+ @brief Pass two string values to multiply and return the value as stwo decimal point double
+ @return
+ */
 -(NSString *) multiplyTwoItems2DecDoubleString :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [NSString stringWithFormat:@"%.2f",[itemOne doubleValue] * [itemTwo doubleValue]];
 }
 
 #pragma mark
-//Pass two string values to multiply and return the value as full double
+/*!
+ @brief Pass two string values to multiply and return the value as full double
+ @return
+ */
 -(NSString *) multiplyTwoItemsFullDoubleString :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [NSString stringWithFormat:@"%f",[itemOne doubleValue] * [itemTwo doubleValue]];
 }
 
 #pragma mark Add Two Strings as Integers
-//Pass two string values to be added as integers and return the result as a string
+/*!
+ @brief Pass two string values to be added as integers and return the result as a string
+ @return
+ */
 -(NSString *) AddTwoItemsAsIntegerString :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [NSString stringWithFormat:@"%d",[itemOne intValue] + [itemTwo intValue]];
 }
 
 #pragma mark Add Two Strings as Double Two Decimal
-//Pass two string values to be added as doubles and return the result as a string
+/*!
+ @brief Pass two string values to be added as doubles and return the result as a string
+ @return
+ */
 -(NSString *) AddTwoItemsAsDoubleString :(NSString *) itemOne :(NSString *) itemTwo
 {
     return [NSString stringWithFormat:@"%f",[itemOne doubleValue] + [itemTwo doubleValue]];
