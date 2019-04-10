3dd4ef30980bea691fd335936210566e08af873b
Joe M <joe.mireles@burnsoft.net>
2019-04-10 10:46:04 -0400

    #75 Enabled 1.4 upgrade and set the database that distributes with the application to journal off and db version 1.4

--- MyEssentialOilRemedies/MYSettings.h
+++ MyEssentialOilRemedies/MYSettings.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 #define MYDBNAME "MEO.db"       //Database Name
-#define MYDBVERSION 1.3         //Expected Database Version for the current version of this app
+#define MYDBVERSION 1.4         //Expected Database Version for the current version of this app
 static BOOL BUGGERME = YES;
 //#warning TODO: ON RELEASE COMPILE LITE AND REGULAR VERSION
 static BOOL ISLITE = NO;
