03d6765bbba8194e59775f2dabd4d0f2e4fc6b62
Joe M <joe.mireles@burnsoft.net>
2020-01-20 16:52:26 -0500

    #113 fully commented out abort and checked to see if any more was in the system

--- MyEssentialOilRemedies/AppDelegate.m
+++ MyEssentialOilRemedies/AppDelegate.m
@@ -129,7 +129,7 @@
             // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
 #ifndef NDEBUG
-            abort();
+            //abort();
 #endif
         }
     }
