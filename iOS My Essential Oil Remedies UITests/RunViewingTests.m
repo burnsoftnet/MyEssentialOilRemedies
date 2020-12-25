cd47c94779457c075e2e3182d722e081661c0250
Joe M <joe.mireles@burnsoft.net>
2020-12-25 16:19:28 -0500

    #117 Added View Oils Test

---
+++ iOS My Essential Oil Remedies UITests/RunViewingTests.m
@@ -0,0 +1,61 @@
+//
+//  RunViewingTests.m
+//  iOS My Essential Oil Remedies UITests
+//
+//  Created by burnsoft on 12/25/20.
+//  Copyright © 2020 burnsoft. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RunViewingTests : XCTestCase
+
+@end
+
+@implementation RunViewingTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testViewOils {
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElementQuery *tablesQuery = app.tables;
+    [tablesQuery.otherElements[@"Section index"] tap];
+    [tablesQuery/*@START_MENU_TOKEN@*/.staticTexts[@"Rosemary"]/*[[".cells.staticTexts[@\"Rosemary\"]",".staticTexts[@\"Rosemary\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ tap];
+    
+    XCUIElementQuery *scrollViewsQuery = app.scrollViews;
+    XCUIElement *toolbarElement = [scrollViewsQuery.otherElements containingType:XCUIElementTypeToolbar identifier:@"Toolbar"].element;
+    [toolbarElement swipeUp];
+    [toolbarElement swipeUp];
+    
+    XCUIElementQuery *elementsQuery = scrollViewsQuery.otherElements;
+//    [elementsQuery.textViews.textViews[@"Used in shampoos to enhance the color of dark hair, counters split ends, and reduces static charge. Great in massage oils and in the bath. Said to aid the memory."] swipeUp];
+    [[[[scrollViewsQuery.otherElements containingType:XCUIElementTypeToolbar identifier:@"Toolbar"] childrenMatchingType:XCUIElementTypeTextView] elementBoundByIndex:2] tap];
+    [toolbarElement tap];
+    [toolbarElement swipeDown];
+    [toolbarElement swipeDown];
+    
+    XCUIElement *toolbar = elementsQuery.toolbars[@"Toolbar"];
+    [toolbar.buttons[@"Related Remedies"] tap];
+    [toolbar.buttons[@"Description"] tap];
+    [app.navigationBars[@"View Oil Details"].buttons[@"Oils"] tap];
+    
+}
+
+- (void)testViewRemedies {
+}
+@end
